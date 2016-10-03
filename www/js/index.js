305aa42c8d274799b98b4eb23cdbc9594389d7f5
Anuj-786 <anuj.k@gai.co.in>
2016-10-03 15:31:43 +0530

    generating card. TODO: Create JPG/PNG

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -38,37 +38,69 @@ var app = {
 
     },
     showValues: function () {
-       var name = document.getElementById("userName").value;
-       document.getElementById("name").innerHTML = "Name:"  + "  " +  name;
+        document.getElementById("mySidenav").style.width="100%";
+        var name = document.getElementById("userName").value;
+        document.getElementById("name").innerHTML = "Name:"  + "  " +  name;
 
-       var dob = document.getElementById("dob").value;
-       document.getElementById("date_of_birth").innerHTML = "D.O.B:" + " " + dob;
+        var dob = document.getElementById("dob").value;
+        document.getElementById("date_of_birth").innerHTML = "D.O.B:" + " " + dob;
 
-       var residence = document.getElementById("residence_of").value;
-       document.getElementById("residence").innerHTML = "Residence:" + " " + residence
+        var dob = document.getElementById("address").value;
+        document.getElementById("residence").innerHTML = "Residence:" + " " + dob;
 
-       var student_non = document.getElementById("student_non").value;
-       document.getElementById("student_non").innerHTML ="Student/Non:" + " " + student_non
+        var student_non = document.getElementById("student_Non").value;
+        document.getElementById("student_non").innerHTML ="Student/Non:" + " " + student_non
 
-       var game = document.getElementById("game").value;
-       document.getElementById("Game").innerHTML = "Game:" + " " + game
+        var game = document.getElementById("game").value;
+        document.getElementById("Game").innerHTML = "Game:" + " " + game
 
-       var contact = document.getElementById("number").value;
-       document.getElementById("contact").innerHTML = "Mob/Ph:" + " " + contact
+        var contact = document.getElementById("number").value;
+        document.getElementById("contact").innerHTML = "Mob/Ph:" + " " + contact
 
-        var timing = document.getElementById("mytime").value;
-       document.getElementById("timing").innerHTML = "Timing" + " " + timing
+        var time= document.getElementById("time").value;
+        document.getElementById("timing").innerHTML = "Timing" + " " + time
 
-       var image = document.getElementById("userImage").src;
+        var image = document.getElementById("userImage").src;
+        var profile = document.getElementById("profile_image");
 
-       var showImage = document.createElement("IMG");
+        var showImage = document.createElement("IMG");
         showImage.className = 'user_uplodad_img'
         showImage.setAttribute("src", image);
-        document.body.appendChild(showImage);
+        var x = profile.appendChild(showImage)
+        
+       
+        //creating canvas of file
+        html2canvas(document.body .sidenav, {
+            onrendered: function(canvas) {
+                var can = document.getElementById("canvas");
+
+                document.body.appendChild(canvas);
+            
+            }
+             
+        });
       
-       return false;
+
+
+        return false;
     },
 
+    fileSystem: function () {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+
+            console.log('file system open: ' + fs.name);
+            createFile(fs.root, 'newTempFile.txt', false)
+            fs.root.getFile("newPersistentFile.txt", { create: true, exclusive: false }, function (fileEntry) {
+
+                console.log("fileEntry is file?" + fileEntry.isFile.toString());
+                // fileEntry.name == 'someFile.txt'
+                // fileEntry.fullPath == '/someFile.txt'
+                writeFile(fileEntry, null);
+
+            }, onErrorCreateFile);
+
+        }, onErrorLoadFs);
+    }
 };
 
 app.initialize();

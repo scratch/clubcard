f0fc4f4c1998368bec05154d38d355fd25b08c82
Anuj-786 <anuj.k@gai.co.in>
2016-10-05 10:20:40 +0530

    working version. Generating image on directory. TODO: bug with save

--- www/js/index.js
+++ www/js/index.js
@@ -35,72 +35,141 @@
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         // app.receivedEvent('deviceready');
-
-    },
-    showValues: function () {
-        document.getElementById("mySidenav").style.width="100%";
-        var name = document.getElementById("userName").value;
-        document.getElementById("name").innerHTML = "Name:"  + "  " +  name;
-
-        var dob = document.getElementById("dob").value;
-        document.getElementById("date_of_birth").innerHTML = "D.O.B:" + " " + dob;
-
-        var dob = document.getElementById("address").value;
-        document.getElementById("residence").innerHTML = "Residence:" + " " + dob;
-
-        var student_non = document.getElementById("student_Non").value;
-        document.getElementById("student_non").innerHTML ="Student/Non:" + " " + student_non
-
-        var game = document.getElementById("game").value;
-        document.getElementById("Game").innerHTML = "Game:" + " " + game
-
-        var contact = document.getElementById("number").value;
-        document.getElementById("contact").innerHTML = "Mob/Ph:" + " " + contact
-
-        var time= document.getElementById("time").value;
-        document.getElementById("timing").innerHTML = "Timing" + " " + time
-
-        var image = document.getElementById("userImage").src;
-        var profile = document.getElementById("profile_image");
-
-        var showImage = document.createElement("IMG");
-        showImage.className = 'user_uplodad_img'
-        showImage.setAttribute("src", image);
-        var x = profile.appendChild(showImage)
         
-       
-        //creating canvas of file
-        html2canvas(document.body .sidenav, {
-            onrendered: function(canvas) {
-                var can = document.getElementById("canvas");
-
-                document.body.appendChild(canvas);
-            
-            }
-             
-        });
-      
-
-
-        return false;
+        var dir = new DirManager;
+         dir.create('/clubcard', Log('created successfully'), Log('something went wrong'));
     },
+    getvalues: function () {
+        // Getting the value of your text input
+        var username = document.getElementById("userName").value;
+            // Storing the value above into localStorage
+            localStorage.setItem("userName", username);
 
-    fileSystem: function () {
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+            var dateOfBirth = document.getElementById("dob").value;
+            localStorage.setItem("dob", dateOfBirth);
 
-            console.log('file system open: ' + fs.name);
-            createFile(fs.root, 'newTempFile.txt', false)
-            fs.root.getFile("newPersistentFile.txt", { create: true, exclusive: false }, function (fileEntry) {
+            var addrr = document.getElementById("address").value;
+            localStorage.setItem("address", addrr);
 
-                console.log("fileEntry is file?" + fileEntry.isFile.toString());
-                // fileEntry.name == 'someFile.txt'
-                // fileEntry.fullPath == '/someFile.txt'
-                writeFile(fileEntry, null);
+            var student_non  = document.getElementById("student_Non").value;
+            localStorage.setItem("student_Non", student_non );
 
-            }, onErrorCreateFile);
+            var Game = document.getElementById("game").value;
+            localStorage.setItem("game", Game);
 
-        }, onErrorLoadFs);
+            var number = document.getElementById("number").value;
+            localStorage.setItem("number", number);
+
+
+            var time = document.getElementById("time").value;
+            localStorage.setItem("time", time);
+
+            
+            
+            // localStorage.setItem("upload_image", image);
+
+
+            var imageStore = document.getElementById("blah").src;
+
+            localStorage.setItem("userImage", imageStore);
+
+
+            return true;
+        },
+
+        init: function () {
+
+            document.getElementById("username").innerHTML = localStorage.getItem("userName");
+            document.getElementById("DOB").innerHTML = localStorage.getItem("dob");
+            document.getElementById("Adress").innerHTML = localStorage.getItem("address");
+            document.getElementById("Student_Nom").innerHTML = localStorage.getItem("student_non");
+            document.getElementById("game").innerHTML = localStorage.getItem("game");
+            document.getElementById("number").innerHTML = localStorage.getItem("number");
+            document.getElementById("time").innerHTML = localStorage.getItem("time");
+
+            var imageUrl = localStorage.getItem("userImage");
+
+            var getImage = document.getElementById("image");
+
+            var showImage = document.createElement("IMG");
+            showImage.className = 'user_uplodad_img';
+            showImage.id = "usr_image";
+            showImage.setAttribute("src", imageUrl);
+            var show = getImage.appendChild(showImage)
+            
+            // html2canvas(document.body, {
+            //     onrendered: function(canvas) {
+            //         var can = document.getElementById("canvas")
+            //         // can.appendChild(canvas);
+            //         // var canvas = document.getElementById('example'),
+            //             dataUrl = canvas.toDataURL("jpeg"),
+            //             imageFoo = document.createElement('img');
+            //         imageFoo.src = dataUrl;
+
+            //         // Style your image here
+            //         imageFoo.style.width = '100%';
+            //         imageFoo.style.height = '100%';
+
+            //     // After you are done styling it, append it to the BODY element
+            //     document.body.appendChild(imageFoo);
+            //         }
+            // });
+        },
+
+        generateCanvasImage: function() {
+             html2canvas(document.body, {
+                onrendered: function(canvas) {
+                    var can = document.getElementById("canvas")
+                    // can.appendChild(canvas);
+                    // var canvas = document.getElementById('example'),
+                        dataUrl = canvas.toDataURL("image/jpeg;base64;"),
+                        imageFoo = document.createElement('img');
+                    imageFoo.src = dataUrl;
+
+                    // Style your image here
+                    imageFoo.style.width = '100%';
+                    imageFoo.style.height = '100%';
+
+                    // After you are done styling it, append it to the BODY element
+
+                    can.appendChild(imageFoo);
+                    var file = new FileManager;
+                    // file.download_file(imageFoo.src, 'clubcard', "img.jpeg", log("success"));
+                    //alert("hello")
+                    file.download_file('http://vignette3.wikia.nocookie.net/harrypotter/images/7/79/Kreacher.img.jpg','/clubcard','Kreacher.img.jpg',Log('downloaded sucess'));
+                },
+                background: '#FFF'
+            });
+
+        },
+
+        fileSystem: function () {
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+
+                console.log('file system open: ' + fs.name);
+                createFile(fs.root, 'newTempFile.txt', false)
+                fs.root.getFile("newPersistentFile.txt", { create: true, exclusive: false }, function (fileEntry) {
+
+                    console.log("fileEntry is file?" + fileEntry.isFile.toString());
+                    // fileEntry.name == 'someFile.txt'
+                    // fileEntry.fullPath == '/someFile.txt'
+                    writeFile(fileEntry, null);
+
+                }, onErrorCreateFile);
+
+            }, onErrorLoadFs);
+        },
+
+        readURL:function(input) {
+          if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                document.getElementById("blah").src = e.target.result;
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
     }
+
 };
 
 app.initialize();

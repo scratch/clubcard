fbee8751037840c4bb1f45f37a74f1994aa32c48
Anuj-786 <anuj.k@gai.co.in>
2016-10-13 18:17:15 +0530

    Working Version. Edit options has been added for form & clean up the code

--- www/js/index.js
+++ www/js/index.js
@@ -37,11 +37,11 @@
         // app.receivedEvent('deviceready');
         
         var dir = new DirManager;
-         dir.create('/clubcard', Log('created successfully'), Log('something went wrong'));
+        dir.create('/clubcard', Log('created successfully'), Log('something went wrong'));
     },
 
     drawer: function () {
-        document.getElementById("drawer").style.width = "100%";
+        document.getElementById("drawer").style.width = "50%";
         
     },
 
@@ -51,14 +51,14 @@
 
     getvalues: function () {
         // Getting the value of your text input
-        var username = document.getElementById("userName").value;
+            var username =  document.getElementById("userName").value;
             // Storing the value above into localStorage
             localStorage.setItem("userName", username);
 
-            var dateOfBirth = document.getElementById("dob").value;
+            var dateOfBirth = "Date of Birth :" + document.getElementById("dob").value;
             localStorage.setItem("dob", dateOfBirth);
 
-            var addrr = document.getElementById("address").value;
+            var addrr = "address" + document.getElementById("address").value;
             localStorage.setItem("address", addrr);
 
             var student_non  = document.getElementById("student_Non").value;
@@ -80,15 +80,27 @@
 
 
             var imageStore = document.getElementById("blah").src;
-
             localStorage.setItem("userImage", imageStore);
 
+            var editLogo = document.getElementById("logo_edit").src;
+            localStorage.setItem("logo_edit", editLogo);
+
+            var organisation = document.getElementById("organisation").innerHTML;
+            localStorage.setItem("organisation", organisation);
+
+            var title = document.getElementById("title").innerHTML;
+            localStorage.setItem("title", title);
+
+            var cardType = document.getElementById("cardType").innerHTML;
+            localStorage.setItem("cardType", cardType);
 
             return true;
         },
 
         init: function () {
-
+            document.getElementById("organiztion").innerHTML = localStorage.getItem("organisation")
+            document.getElementById("print-title").innerHTML = localStorage.getItem("title");
+            document.getElementById("print-card").innerHTML = localStorage.getItem("cardType");
             document.getElementById("username").innerHTML = localStorage.getItem("userName");
             document.getElementById("DOB").innerHTML = localStorage.getItem("dob");
             document.getElementById("Adress").innerHTML = localStorage.getItem("address");
@@ -105,36 +117,27 @@
             showImage.className = 'user_uplodad_img';
             showImage.id = "usr_image";
             showImage.setAttribute("src", imageUrl);
-            var show = getImage.appendChild(showImage)
-            
-            // html2canvas(document.body, {
-            //     onrendered: function(canvas) {
-            //         var can = document.getElementById("canvas")
-            //         // can.appendChild(canvas);
-            //         // var canvas = document.getElementById('example'),
-            //             dataUrl = canvas.toDataURL("jpeg"),
-            //             imageFoo = document.createElement('img');
-            //         imageFoo.src = dataUrl;
+            var show = getImage.appendChild(showImage);
 
-            //         // Style your image here
-            //         imageFoo.style.width = '100%';
-            //         imageFoo.style.height = '100%';
+            var updatedImgUrl = localStorage.getItem("logo_edit");
+            var createUpdatedImage = document.getElementById("logo");
 
-            //     // After you are done styling it, append it to the BODY element
-            //     document.body.appendChild(imageFoo);
-            //         }
-            // });
+            var showUpdatedImage = document.createElement("IMG");
+            showUpdatedImage.className = "updatedImage";
+            showUpdatedImage.id = "updatedImageShow";
+            showUpdatedImage.setAttribute("src", updatedImgUrl);
+            var displayUpdated = createUpdatedImage.appendChild(showUpdatedImage)
         },
 
         generateCanvasImage: function() {
-             html2canvas(document.body, {
-                onrendered: function(canvas) {
-                    var can = document.getElementById("canvas")
+         html2canvas(document.body, {
+            onrendered: function(canvas) {
+                var can = document.getElementById("canvas")
                     // can.appendChild(canvas);
                     // var canvas = document.getElementById('example'),
-                        dataUrl = canvas.toDataURL("image/jpeg;base64;"),
+                    dataUrl = canvas.toDataURL("image/jpeg;base64;"),
 
-                        imageFoo = document.createElement('img');
+                    imageFoo = document.createElement('img');
                     imageFoo.src = dataUrl;
 
                     // Style your image here
@@ -155,34 +158,67 @@
                 background: '#FFF'
             });
 
-        },
+     },
 
-            fileSystem: function () {
-            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+     fileSystem: function () {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
 
-                console.log('file system open: ' + fs.name);
-                createFile(fs.root, 'newTempFile.txt', false)
-                fs.root.getFile("newPersistentFile.txt", { create: true, exclusive: false }, function (fileEntry) {
+            console.log('file system open: ' + fs.name);
+            createFile(fs.root, 'newTempFile.txt', false)
+            fs.root.getFile("newPersistentFile.txt", { create: true, exclusive: false }, function (fileEntry) {
 
-                    console.log("fileEntry is file?" + fileEntry.isFile.toString());
+                console.log("fileEntry is file?" + fileEntry.isFile.toString());
                     // fileEntry.name == 'someFile.txt'
                     // fileEntry.fullPath == '/someFile.txt'
                     writeFile(fileEntry, null);
 
                 }, onErrorCreateFile);
 
-            }, onErrorLoadFs);
-        },
+        }, onErrorLoadFs);
+    },
 
-        readURL:function(input) {
+    editHeading: function () {
+        var organisation = prompt("Enter Your organisation name", "Dept of Youth Services & Sports H.P");
+        document.getElementById("organisation").innerHTML = organisation;
+    },
+
+    editTitle: function () {
+        var title = prompt("Enter your Title", "Indoor Stadium Dharamshala");
+        document.getElementById("title").innerHTML = title;
+    },
+    
+    editCard: function() {
+        var cardType = prompt("Enter your Card Type", "Identity card ");
+        document.getElementById("cardType").innerHTML = cardType;
+    },
+
+    readURLForProfile:function(input) {
           if (input.files && input.files[0]) {
             var reader = new FileReader();
             reader.onload = function (e) {
                 document.getElementById("blah").src = e.target.result;
             };
-            reader.readAsDataURL(input.files[0]);
+            reader.readAsDataURL(input.files[0]);   
         }
-    }
+    },
+
+    readURLForEditLogo:function(input) {
+          if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                document.getElementById("logo_edit").src = e.target.result;
+            };
+            reader.readAsDataURL(input.files[0]);   
+        }
+    },
+
+    // edit: function () {
+
+        
+    //      var text = prompt("Crete your values", "test")
+    //      var a = document.getElementById("testname").innerHTML = text;
+    // },
+
 
 };
 
